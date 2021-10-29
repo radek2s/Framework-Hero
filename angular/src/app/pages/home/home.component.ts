93ba1270d513a103d42af116d6ef25843b6dff7b
radek2s <radek235s@gmail.com>
2021-10-29 17:45:20 +0000

    Deploying to master from  @ f26ed2a70676102211df41dc2867313d7108fa60 🚀

--- angular/src/app/pages/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
- 
-@Component({
-  selector: 'home',
-  templateUrl: './home.component.html', //templateUrl instead of template
-})
-export class HomeComponent {
-  constructor() { }
-}

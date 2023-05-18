c8c2f92a023a89c7b1cb440eafa791b60ce7fd44
radek2s <radek235s@gmail.com>
2023-05-18 19:27:51 +0200

    ⚰️ #11 Remove shell app

--- shell/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { loadRemoteEntry } from '@angular-architects/module-federation';
-
-Promise.all([
-	loadRemoteEntry({type: 'module', remoteEntry: "http://localhost:3000/remoteEntry.js"})
-])
-.catch(err => console.error("Failed to load remote entries", err))
-.then(() => import('./bootstrap'))
-.catch(err => console.error(err))
-

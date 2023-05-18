7a9209f0b4402738278b2a3b35e4ca9ccacf6da7
radek2s <radek235s@gmail.com>
2023-05-19 01:21:15 +0200

    #11 💻 Improve navigation

--- root/src/Root.jsx
+++ root/src/Root.jsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router"
+import './styles.css'
 
 export const Root = () => {
-    return (<div>
-            <h1>Hello MicroFrontends</h1>
+    const navigateTo = (path) => {
+        const {
+            protocol,
+            host
+        } = window.location
+        console.log({protocol, host})
+        window.location = `${protocol}//${host}/#/${path}`
+    }
+    return (<div className="shell-navbar">
+        <ul>
+            <li onClick={() => navigateTo("angular")}>Angular</li>
+            <li onClick={() => navigateTo("react")}>React</li>
+            <li onClick={() => navigateTo("vue")}>Vue</li>
+        </ul>
             <Outlet/>
         </div>)
 
